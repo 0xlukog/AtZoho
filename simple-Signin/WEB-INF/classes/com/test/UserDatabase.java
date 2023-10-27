f28a257d558db792ee3250a27a8722505fb69dcc
Gokul R <75027527+0xlukog@users.noreply.github.com>
2023-10-27 12:37:55 +0530

    a simple signin server application using servlets

---
+++ simple-Signin/WEB-INF/classes/com/test/UserDatabase.java
@@ -0,0 +1,9 @@
+package com.test;
+
+import java.util.HashMap;
+
+public class UserDatabase {
+    public static HashMap<String,String> userDataBase =new HashMap<>();
+    public static HashMap<String,String> cookieDataBase =new HashMap<>();
+
+}
